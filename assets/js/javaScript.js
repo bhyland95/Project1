e05491fd7eaa0c0cab853dee18f35e6083a1df63
BH <blakehyland95@gmail.com>
2021-09-03 23:31:20 -0600

    hooked up inputs into API

--- assets/js/javaScript.js
+++ assets/js/javaScript.js
@@ -1,14 +1,41 @@
-var outboundDate = "2021-09-10"
-var returnDate = "2021-09-15"
-var currency = "USD"
-var language = "en-US"
-var country = "US"
-var origin = "US"
-var destination = "US"
+//ON CLICK OF SUBMIT BUTTON
+$("#search-btn").on("click", function () {
+	var currency = "USD"
+	var language = "en-US"
+	var country = "US"
+	//grabs outbound date
+	var outboundDate = $("#outboundDate").val()
+	console.log(outboundDate)
+	if (outboundDate == "") {
+		alert("Please enter in a OutBound Date")
+		return
+	}
+	//grabs return date
+	var returnDate = $('#returnDate').val()
+	console.log(returnDate)
+	if (returnDate == "") {
+		alert("Please enter in a Return Date")
+		return
+	}
+	//grabs origin
+	var origin = $('#Origin').val()
+	console.log(origin)
+	if (origin == "") {
+		alert("Please enter in an origin")
+		return
+	}
+	//grabs destination
+	var destination = $('#Destination').val()
+	console.log(destination)
+	if (destination == "") {
+		alert("Please enter in a Destination")
+		return
+	}
 
 
-//SKYSCANNER API that returns routes from an origin to a destination at a specific date 
-fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browseroutes/v1.0/" + country + "/"+ currency +"/" +language + "/" + origin + "/" + destination + "/" + outboundDate + "?inboundpartialdate=" + returnDate,{
+	//throws inputs into API 
+	//SKYSCANNER API that returns routes from an origin to a destination at a specific date 
+fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browseroutes/v1.0/" + country + "/"+ currency +"/" +language + "/" + origin + "/" + destination + "/" + outboundDate + "?inboundpartialdate=" + returnDate ,{
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
@@ -17,3 +44,5 @@ fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices
 })
 .then(response => response.json())
 .then(data => console.log(data));
+
+});
