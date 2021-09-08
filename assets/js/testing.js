8b3698c578a3f3c83660427b0674f29d29c9dc95
bhyland <blakehyland95@gmail.com>
2021-09-07 19:27:10 -0600

    small fixes

--- assets/js/testing.js
+++ assets/js/testing.js
@@ -1,9 +1,10 @@
-fetch("https://partners.api.skyscanner.net/apiservices/pricing/v1.0" , {
-		"method": "GET",
-		"headers": {
-			"x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-			"x-rapidapi-key": "dae9f598fbmsh6d16f4dfa6f12a7p1e4403jsnee920035895e"
-		}
-	})
-		.then(response => response.json())
-		.then(data => console.log(data))
+fetch("https://predicthq.p.rapidapi.com/v1/events/?place.scope=LAX&active.gte=2021-09-07&active.lte=2021-09-14&category=concerts,festivals,performing-arts,sports&sort=rank", {
+    "method": "GET",
+    "headers": {
+        "authorization": "Bearer vjK8YdM4-zWiaUZ5HqhW48f7vsVWdPaeklGDxUQf",
+        "x-rapidapi-host": "predicthq.p.rapidapi.com",
+        "x-rapidapi-key": "dae9f598fbmsh6d16f4dfa6f12a7p1e4403jsnee920035895e"
+    }
+})
+.then(response => response.json())
+.then(data => console.log(data))
